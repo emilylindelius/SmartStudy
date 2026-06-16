f9bee6fbd4ff35b6eca52e2994fcfe0f110f145a
elvis <elvis@IT481PC>
2026-06-16 16:52:10 -0700

    GitHub Structural Fix

--- Sprint1SmartStudy/SmartStudyPlanner.java
+++ Sprint1SmartStudy/SmartStudyPlanner.java
@@ -1,3 +1,4 @@
+package Sprint1SmartStudy;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
--- Sprint1SmartStudy/TaskAppTest.java
+++ Sprint1SmartStudy/TaskAppTest.java
@@ -1,3 +1,4 @@
+package Sprint1SmartStudy;
 import java.io.File;
 import java.io.IOException;
 
